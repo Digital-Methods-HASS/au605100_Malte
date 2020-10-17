7788913d18c612bca1e166c0015f7de6026fb60f
Malte Højmark-Bertelsen <malte@hoejmark-bertelsen.dk>
2020-10-17 17:34:55 +0200

    major git fuck up

--- Assignment4.R
+++
@@ -1,4 +0,0 @@
-install.packages("tidyverse")
-library(tidyverse)
-download.file("https://ndownloader.figshare.com/files/11492171", "data/SAFI_clean.csv", mode = "wb")
-df <- read.csv("data/SAFI_clean.csv")
