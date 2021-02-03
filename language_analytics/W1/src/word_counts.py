08a1fe27d80d40df88e05378027bd260232e98c3
hjb <hjb@kmd.dk>
2021-02-03 16:44:50 +0100

    Created classes and functions

--- language_analytics/W1/src/word_counts.py
+++ language_analytics/W1/src/word_counts.py
@@ -3,48 +3,147 @@ import os
 
 from pathlib import Path
 import pandas as pd
+import argparse
 
-root_dir = Path.cwd()  # Setting root directory.
 
-data_dir = root_dir / "data" / "100_english_novels" / "corpus"  # Setting data directory.
+def main(args):
 
-files = [file for file in data_dir.glob("*.txt") if file.is_file()]  # Using list comprehension to get all the file names if they are files.
+    data_dir = args.data_dir
 
-# Creating empty lists
+    WordCounts(data_dir)
 
-filename = []
 
-total_words = []
+class WordCounts:
 
-unique_words = []
+    def __init__(self, data_dir=None):
 
-# Loop for iterating through the different files.
-for file in files:
+        self.data_dir = data_dir
 
-    novel_file_name = os.path.split(file)[-1]  # I take the last snippet of the path, which is the novel filename.
+        if data_dir is None:
 
-    filename.append(novel_file_name)  # Append each filename to the list.
+            data_dir = self.setting_default_data_dir()
 
-    # Read each file.
-    with open(file, encoding="utf-8") as f:
+        files = self.get_filepaths_from_data_dir(data_dir)
 
-        novel = f.read()
+        filenames = self.get_filenames(files)
 
-        f.close()
+        total_words = self.get_total_words(files)
 
-    tokens = novel.split()  # I split the tokens by whitespace. Do note that to get a proper tokenization, more clever solutions could be applied. See fx NLTK's tokenizer.
+        unique_words = self.get_total_unique_words(files)
 
-    total_words.append(len(tokens))  # I append the length of the tokens from each novel.
+        df = self.get_pandas_dataframe(filenames=filenames,
+                                       total_words=total_words,
+                                       unique_words=unique_words)
 
-    unique_tokens = set(tokens)  # Create a set of unique tokens.
+        dataframe_path = "word_counts.csv"
 
-    unique_words.append(len(unique_tokens))  # I append the number of unique tokens from each novel.
+        df.to_csv(dataframe_path)  # Writing data to csv.
 
-# Creating Python dictionary to use for pandas. If there are more efficient ways to create a pandas DataFrame from scratch - please let me know :-)
-data_dict = {"filename": filename,
-             "total_words": total_words,
-             "unique_words": unique_words}
+        print(f"DONE! Created the file: '{dataframe_path}'. Have a nice day. :-)")
 
-df = pd.DataFrame(data=data_dict)
+    def setting_default_data_dir(self):
 
-df.to_csv("word_counts.csv")  # Writing data to csv.
+        root_dir = Path.cwd()  # Setting root directory.
+
+        data_dir = root_dir / "data" / "100_english_novels" / "corpus"  # Setting data directory.
+
+        return data_dir
+
+    def get_filepaths_from_data_dir(self, data_dir):
+        """Creates a list containing paths to filenames in a data directoryl
+
+        Args:
+            data_dir (PosixPath): PosixPath to the data directory.
+        """
+
+        files = [file for file in data_dir.glob("*.txt") if file.is_file()]  # Using list comprehension to get all the file names if they are files.
+
+        return files
+
+    def get_filenames(self, files):
+        """Creates a list of filenames in a directory.
+
+        Args:
+            files (list): List of file paths
+
+        Returns:
+            filename: list of filenames
+        """
+
+        filename = []  # Creating empty list
+
+        # Loop for iterating through the different files.
+        for file in files:
+
+            novel_file_name = os.path.split(file)[-1]  # I take the last snippet of the path, which is the novel filename.
+
+            filename.append(novel_file_name)  # Append each filename to the list.
+
+        return filename
+
+
+    def get_total_words(self, files):
+
+        total_words = []  # Creating empty list
+
+        # Loop for iterating through the different files.
+        for file in files:
+
+            # Read each file.
+            with open(file, encoding="utf-8") as f:
+
+                novel = f.read()
+
+                f.close()
+
+            tokens = novel.split()  # I split the tokens by whitespace. Do note that to get a proper tokenization, more clever solutions could be applied. See fx NLTK's tokenizer.
+
+            total_words.append(len(tokens))  # I append the length of the tokens from each novel.
+
+        return total_words
+
+    def get_total_unique_words(self, files):
+
+        unique_words = []  # Creating empty list
+
+        # Loop for iterating through the different files.
+        for file in files:
+
+            # Read each file.
+            with open(file, encoding="utf-8") as f:
+
+                novel = f.read()
+
+                f.close()
+
+            tokens = novel.split()  # I split the tokens by whitespace. Do note that to get a proper tokenization, more clever solutions could be applied. See fx NLTK's tokenizer.
+
+            unique_tokens = set(tokens)  # Create a set of unique tokens.
+
+            unique_words.append(len(unique_tokens))  # I append the number of unique tokens from each novel.
+
+        return unique_words
+
+    def get_pandas_dataframe(self, filenames, total_words, unique_words):
+
+        # Creating Python dictionary to use for pandas. If there are more efficient ways to create a pandas DataFrame from scratch - please let me know :-)
+        data_dict = {"filename": filenames,
+                     "total_words": total_words,
+                     "unique_words": unique_words}
+
+        df = pd.DataFrame(data=data_dict)
+
+        return df
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir',
+                        metavar="data_dir",
+                        type=str,
+                        help='A PosixPath to the data directory',
+                        required=False)
+
+    main(parser.parse_args())
